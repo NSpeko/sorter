135e79771e4dd6fe032d0e00651e704d0b6f26c0
NSpeko <nonstoper42@gmail.com>
2018-02-25 20:15:09 +0300

    Feature: weird class was implemented

--- src/index.js
+++ src/index.js
@@ -1,30 +1,46 @@
 class Sorter {
   constructor() {
     // your implementation
+      this.innerArr=[];
+      this.comparatorFunc=function (num1,num2){return num1-num2;};
   }
 
   add(element) {
     // your implementation
+      this.innerArr.push(element);
   }
 
   at(index) {
     // your implementation
+      return this.innerArr[index];
   }
 
   get length() {
     // your implementation
+      return this.innerArr.length;
   }
 
   toArray() {
     // your implementation
+      return this.innerArr;
   }
 
   sort(indices) {
     // your implementation
+      let tempArr=[];
+      for(let i=0;i<indices.length;i++){
+          tempArr.push(this.innerArr[indices[i]]);
+      }
+      tempArr.sort(this.comparatorFunc);
+      indices.sort((num1,num2)=>{return num1-num2;});
+      for(let i=0;i<indices.length;i++){
+          this.innerArr.splice(indices[i],1,tempArr[i]);
+      }
   }
 
   setComparator(compareFunction) {
     // your implementation
+      this.comparatorFunc=compareFunction;
   }
 }
 
